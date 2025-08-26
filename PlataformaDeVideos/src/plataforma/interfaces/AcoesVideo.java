bd1c717729e7ba0ce98e79983f6e393341db13c4
Jenifer <jenifergonbrito@gmail.com>
2025-08-26 00:03:14 -0300

    feat: criacao de classe e definindo acoes que um video pode sofrer

---
+++ PlataformaDeVideos/src/plataforma/interfaces/AcoesVideo.java
@@ -0,0 +1,9 @@
+package plataforma.interfaces;
+
+public interface AcoesVideo {
+	public void play();
+	public void pause();
+	public void like();
+	public void compartilhar(); //add
+	public void comentar(); //add
+}
