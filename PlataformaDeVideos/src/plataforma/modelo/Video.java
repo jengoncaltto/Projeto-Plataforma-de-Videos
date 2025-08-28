5066f5f2720a4b18017a249afae47755627c4a84
Gonçalves <jenifergonbrito@gmail.com>
2025-08-28 15:37:24 -0300

    feat: adicionando lista de comentarios; adicionando getters e setters

--- PlataformaDeVideos/src/plataforma/modelo/Video.java
+++ PlataformaDeVideos/src/plataforma/modelo/Video.java
@@ -1,5 +1,8 @@
 package plataforma.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import plataforma.interfaces.AcoesVideo;
 
 public class Video implements AcoesVideo {
@@ -8,7 +11,8 @@ public class Video implements AcoesVideo {
 	private String descricao; // add 
 	private int views;
 	private int curtidas;
-	private int comentarios; //add
+	private int quantidadeComentarios; //add
+	private List<String> listaComentarios; //add
 	private int compartilhamentos;  //add
 	private boolean reproduzindo;
 	private float tempoTotalVideo;  // add 
@@ -21,13 +25,15 @@ public class Video implements AcoesVideo {
 		this.curtidas = 0;
 		this.reproduzindo = false;
 		this.tempoTotalVideo = tempoReproducao;
-		this.comentarios = 0;
+		this.quantidadeComentarios = 0;
 		this.compartilhamentos = 0;
+		this.listaComentarios = new ArrayList<String>();
 	}
 	
 	@Override
-	public void comentar() {
-		this.comentarios++;
+	public void comentar(String comentario) {
+		listaComentarios.add(comentario);
+		this.quantidadeComentarios++;
 	}
 
 	@Override
@@ -97,13 +103,6 @@ public class Video implements AcoesVideo {
 	public void setTempoReproducao(float tempoReproducao) {
 		this.tempoTotalVideo = tempoReproducao;
 	}
-
-	@Override
-	public String toString() {
-		return "Video [titulo=" + titulo + ", descricao=" + descricao + ", views=" + views + ", curtidas=" + curtidas
-				+ ", reproduzindo=" + reproduzindo + ", tempoTotalVideo=" + tempoTotalVideo + "]";
-	}
-
 	public int getAvaliacao() {
 		return avaliacao;
 	}
@@ -112,5 +111,44 @@ public class Video implements AcoesVideo {
 		int nova; 
 		nova = (int) (this.avaliacao + avaliacao/this.views);
 		this.avaliacao = nova;
+	}
+	
+
+	public int getQuantidadeComentarios() {
+		return quantidadeComentarios;
+	}
+
+	public void setQuantidadeComentarios(int quantidadeComentarios) {
+		this.quantidadeComentarios = quantidadeComentarios;
+	}
+
+	public List<String> getListaComentarios() {
+		return listaComentarios;
+	}
+
+	public void setListaComentarios(List<String> listaComentarios) {
+		this.listaComentarios = listaComentarios;
+	}
+
+	public int getCompartilhamentos() {
+		return compartilhamentos;
+	}
+
+	public void setCompartilhamentos(int compartilhamentos) {
+		this.compartilhamentos = compartilhamentos;
+	}
+
+	public float getTempoTotalVideo() {
+		return tempoTotalVideo;
+	}
+
+	public void setTempoTotalVideo(float tempoTotalVideo) {
+		this.tempoTotalVideo = tempoTotalVideo;
+	}
+
+	@Override
+	public String toString() {
+		return "Video [titulo=" + titulo + ", descricao=" + descricao + ", views=" + views + ", curtidas=" + curtidas
+				+ ", reproduzindo=" + reproduzindo + ", tempoTotalVideo=" + tempoTotalVideo + "]";
 	}
 }
